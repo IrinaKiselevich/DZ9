9a4cefad2d21b978eb1deb2f086094000b6a1f2b
unknown <irina.kiselevitch@yandex.ru>
2022-11-01 21:57:09 +0700

    задачи дз 7

---
+++ Program.cs
@@ -0,0 +1,143 @@
+﻿//aka.ms/new-console-template for more information
+// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+// Console.WriteLine("введите m");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите n");
+// int n = Convert.ToInt32(Console.ReadLine());
+// double[,] numbers = new double[m, n];
+// FillArrayNumbers(numbers);
+// PrintArray(numbers);
+
+// void FillArrayNumbers(double[,] array)
+// {
+//    for (int i = 0; i < array.GetLength(0); i++)
+//    {
+//        for (int j = 0; j < array.GetLength(1); j++)
+//        {
+//            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100));
+//         }
+//     }
+// }
+
+// void PrintArray(double[,] array)
+// {
+//    for (int i = 0; i < array.GetLength(0); i++)
+    
+//        for (int j = 0; j < array.GetLength(1); j++)
+//        {
+//            Console.Write($"{array[i, j]} ");
+//         }
+        
+//        Console.Write(" ");
+//        Console.WriteLine(" "); 
+// }
+
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+
+// Console.WriteLine("введите номер строки");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите номер столбца");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int [,] numbers = new int [10,10];
+
+// FillArray(numbers);
+// PrintArray(numbers);
+
+// if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+// {
+//     Console.WriteLine("такого элемента нет");
+// }
+// else
+// {
+//     Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
+// }
+
+// void FillArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//         {        
+//             for (int j = 0; j < array.GetLength(1); j++)
+//             {
+//                 array [i,j] = new Random().Next(-100, 100);
+//             }   
+//         }
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write(" ");
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i,j] + " ");
+//         }   
+//         Console.Write(" ");
+//         Console.WriteLine(" "); 
+//     }
+// }
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+Console.WriteLine("введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[n, m];
+FillArray(array);
+PrintArray(array);
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    double srz =0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = (sum + array[i, j]);
+    }
+    srz = sum / n;
+    Math.Round(srz, 1, MidpointRounding.ToEven);
+
+    Console.Write( srz + "; ");
+}
+Console.WriteLine();
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+       
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+      
+        Console.WriteLine("");
+    }
+}
